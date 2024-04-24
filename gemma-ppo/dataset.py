2964d786b7f8072974065f65683481d189b3e58d
Josue Xavier Torres-Fonseca <josuetf@sled-whistler.eecs.umich.edu>
2024-04-24 09:37:42 -0400

    added dataset

--- gemma-ppo/dataset.py
+++ gemma-ppo/dataset.py
@@ -3,38 +3,30 @@ from torch.utils.data import Dataset, DataLoader
 import csv
 
 
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+device = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
 print(device)
 def get_prompt(scramble):
 
-    return f"""You are a Rubik's cube solving assistant. Your job is to generate the moves needed to solve a Rubik's cube when given the Rubik's cube scramble. A scramble is a list of moves that are performed on a fully solved Rubik's cube in order to scramble it up.
+    return f"""You are a 2x2 Rubik's cube solving assistant. Your job is to generate the space separated moves needed to solve a Rubik's cube when given the Rubik's cube scramble. A scramble is a list of moves that are performed on a fully solved Rubik's cube in order to scramble it up.
 
     Below are the only valid next moves:
-    U (Up): Rotate the upper face 90 degrees clockwise.
-    U' (Up Prime): Rotate the upper face 90 degrees counter-clockwise.
-    U2 (Up twice): Rotate the upper face 180 degrees.
-    D (Down): Rotate the bottom face 90 degrees clockwise.
-    D' (Down Prime): Rotate the bottom face 90 degrees counter-clockwise.
-    D2 (Down twice): Rotate the bottom face 180 degrees.
-    F (Front): Rotate the front face 90 degrees clockwise.
-    F' (Front Prime): Rotate the front face 90 degrees counter-clockwise.
-    F2 (Front twice): Rotate the front face 180 degrees.
-    B (Back): Rotate the back face 90 degrees clockwise.
-    B' (Back Prime): Rotate the back face 90 degrees counter-clockwise.
-    B2 (Back twice): Rotate the back face 180 degrees.
-    L (Left): Rotate the left face 90 degrees clockwise.
-    L' (Left Prime): Rotate the left face 90 degrees counter-clockwise.
-    L2 (Left twice): Rotate the left face 180 degrees.
-    R (Right): Rotate the right face 90 degrees clockwise.
-    R' (Right Prime): Rotate the right face 90 degrees counter-clockwise.
-    R2 (Right twice): Rotate the right face 180 degrees.
+
+    U
+    U'
+    U2
+    F
+    F'
+    F2
+    R
+    R'
+    R2
 
     Here is an example scramble and correct response.
 
-    Scramble: F2 B' U2 D' R2 L' U' B2 U2 B U' L2 U2 L U2 R B2 F2 R2 D
-    Next moves: R D' L' U2 L2 B R' D' B R' F D2 B2 R2 U' F2 U' R2 L2 F2 B2
+    Scramble: F
+    Next moves: F'
 
-    Now you should generate the correct next moves to solve the rubiks cube following the scramble.
+    Now you should generate the correct next move to solve the 2x2 rubiks cube following the scramble. (note: your answer should only contain one move).
     Scramble: {scramble}
     Next moves: """
 
@@ -47,7 +39,7 @@ def load_data(data_path, num_predicted_turns=10000):
         result = []
         for row in reader:
             result.append({
-                'query': row['Scramble (State)'].strip(),
+                'query': row['Scramble'].strip(),
                 'output': ' '.join(row['Solution'].strip().split(' ')[:num_predicted_turns])
             })
 
